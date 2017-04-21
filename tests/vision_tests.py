c084db4729c9bdc126de7d1741c6240b8d2456a0
Dittmar <m181524@academy.usna.edu>
2017-04-20 22:27:30 -0400

    added client 0.0

--- tests/vision_tests.py
+++ tests/vision_tests.py
@@ -1,11 +1,43 @@
+import os
+import subprocess
+import time
+import unittest
 from nose.tools import *
 import vision
 
-def setup():
-    print "SETUP!"
+class VisionCliTestBase(unittest.TestCase):
+    """Base for tests."""
 
-def teardown():
-    print "TEAR DOWN!"
+    def setUp(self):
+        """Setup vision client path and base args."""
+        self.cli_path = os.path.join(
+            os.path.dirname(__file__), "/vision/client.py")
+        self.cli_base_args = [
+            self.cli_path, '--url', 'http://10.17.163.109:8000', '--username',
+            'testuser', '--password', 'testpass'
+        ]
 
-def test_basic():
-    print "I RAN!"
+    def assertCliOk(self, args):
+        """Determines that cli command succeeds."""
+        self.assertEqual(0, subprocess.call(args))
+
+class TestTargets(VisionCliTestBase):
+    """Test able to upload targets."""
+
+    def setUp(self):
+        """Comput the testdata folder."""
+        super(TestTargets, self).setUp()
+        self.target_dir = os.path.join(os.path.dirname(__file__), "/bin/testdata")
+
+    def test_upload_targets(self):
+        """Test uploading targest with object file format."""
+        self.assertCliOk(self.cli_base_args + [
+            'targets', '--target_dir', self.target_dir
+        ])
+
+def main():
+    tests = unittest.TestLoader().loadTestsFromTestCase(TestTargets)
+    unittest.TextTestRunner(verbosity=3).run(tests)
+
+if __name__ == '__main__':
+    main()
---
+++ vision/client.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# make sure you set python path in terminal
+# command: set PYTHONPATH = %PYTHONPATH%;
+# Target Client
+# This is the handler for posting images and targets.
+# Both are manual inputs at this point
+
+from __future__ import print_function
+import argparse
+import datetime
+import getpass
+import logging
+import pprint
+import sys
+import time
+
+from interop import AsyncClient
+from interop import Target
+from upload_targets import upload_taret, upload_legacy_targets
+
+logger = logging.getLogger(__name__)
+
+def targets(args,client):
+    if args.legacy_filepath:
+        if not args.target_dir:
+            raise ValueError('--target_dir is required.')
+        upload_legacy_targets(client, args.legacy_filepath, args.target_dir)
+    elif args.target_dir:
+        upload_targets(client, args.target_dir, args.team_id,
+                       args.actionable_override)
+    else:
+        targets = client.get_targets()
+        for target in targets.result():
+            pprint.pprint(target.serialize())
+
+def main():
+    #initialize logging
+    logging.basicConfig(
+        level=logging.INFO,
+        stream=sys.stdout,
+        format='%(asctime)s: %(name)s: %(levelname)s: %(message)s')
+
+    #parse command line args.
+    parser = argparse.ArgumentParser(description='Async interop CLI.')
+    parser.add_argument('--url',
+                        required=True,
+                        help='URL for server.')
+    parser.add_argument('--username',
+                        required=True,
+                        help='Username for server login.')
+    parser.add_argument('--password',help='Password for server login.')
+
+    subparsers = parser.add_subparsers(help='Sub-command help.')
+
+    subparser = subparsers.add_parser('missions',help='Get Missions.')
+    subparser.set_defaults(func=missions)
+
+    subparser = subparsers.add_parser(
+        'targets',
+        help='Upload Targets.',
+        description='''Download or upload targets to/from the interoperability
+server.
+
+Without extra arguments, this prints all targets that have been uploaded to the
+server.
+
+With --target_dir, this uploads new targets to the server.
+
+This tool searches for target JSON and images files within --target_dir
+conforming to the 2017 Object File Format and uploads the target
+characteristics and thumbnails to the interoperability server.
+
+Alternatively, if --legacy_filepath is specified, that file is parsed as the
+legacy 2016 tab-delimited target file format. Image paths referenced in the
+file are relative to --target_dir.
+
+There is no deduplication logic. Targets will be uploaded multiple times, as
+unique targets, if the tool is run multiple times.''',
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    subparser.set_defaults(func=targets)
+    subparser.add_argument(
+         '--legacy_filepath',
+         help='Target file in the legacy 2016 tab-delimited format.')
+    subparser.add_argument(
+         '--target_dir',
+         help='Enables target upload. Directory containing target data.')
+
+    # Parse args, get password
+    args = parser.parse_args()
+    if args.password:
+        password = args.password
+    else:
+        password = getpass.getpass('Interoperability Password: ')
+
+    # Create client and dispatch subcommand.
+    client = AsyncClient(args.url, args.username, password)
+    args.func(args, client)
+
+if __name__ == '__main__':
+    main()
